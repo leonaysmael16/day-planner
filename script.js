e59b1d48431e569961e481b9609f35637120d3ac
leonaysmael16 <ysmealleona@gmail.com>
2021-09-17 18:15:07 -0700

    updates

--- script.js
+++ script.js
@@ -6,9 +6,14 @@
 
 // shows today's date and current time 
 
-var currentTime = moment().format('MMMM Do YYYY, h:mm');
-$("#currentDay").html(currentTime);
+function setTime() {
+    var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+    $("#currentDay").html(currentTime);
+}
 
+setInterval(setTime, 1000);
+
+setTime()
 // save button
 
 // var btnSave = $(".saveBtn");
@@ -29,9 +34,17 @@ $("#currentDay").html(currentTime);
 
 
 $('.saveBTN').on('click', function () {
-    var time = $(this).parent().attr('id');
-    console.log('time', time);
-    var planner = $(this).siblings('.planner').val();
-    console.log('planner', planner);
-    localStorage.setItem(time, planner);
+    var time = $(this).parent().attr('id');
+    console.log('time', time);
+    var planner = $(this).siblings('.planner').val();
+    console.log('planner', planner);
+    localStorage.setItem(time, planner);
 });
+
+
+// load from local storage
+
+var hour1 = localStorage.getItem("9 am");
+$("#9-am").val(hour1);
+
+var hour2 = localStorage.getItem("10 am");
